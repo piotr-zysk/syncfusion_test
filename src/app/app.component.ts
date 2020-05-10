63b99b33657c849719be0eb03fbb016509ee7be1
Piotr Zysk <pizy123@gmail.com>
2020-05-10 20:36:32 +0200

    syncfusion doc editor sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ToolbarService } from '@syncfusion/ej2-angular-documenteditor';
+ 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ToolbarService]
 })
-export class AppComponent {
-  title = 'angular9-app';
+export class AppComponent implements OnInit{
+  title = 'angular9-app';
+  public serviceLink: string;
+  ngOnInit() {
+    this.serviceLink = 'https://ej2services.syncfusion.com/production/web-services/api/documenteditor/';
+  }
 }
